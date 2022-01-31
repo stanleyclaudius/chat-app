ec93994ce59fd66cfe0eed312b3788cc6c87af13
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-31 15:27:03 +0700

    Create user profile dropdown at Header component

---
+++ client/src/components/global/Header.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { IoChevronBackOutline } from 'react-icons/io5'
+import { FaEdit } from 'react-icons/fa'
+import { MdLogout } from 'react-icons/md'
+import Avatar from './Avatar'
+
+const Header = ({ selectContact, setSelectContact }) => {
+  const [openDropdown, setOpenDropdown] = useState(false)
+
+  return (
+    <div className='flex items-center justify-between border-b-2 py-1 md:px-12 px-4 sticky top-0 z-[50]'>
+      <div className='flex items-center'>
+        <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt='Inspace' width='35' />
+        <h1 className='md:block hidden font-logo text-lg ml-4'>Inspace</h1>
+      </div>
+      <div className='text-center'>
+        <div className='flex items-center'>
+          {selectContact && <IoChevronBackOutline className='md:hidden block translate-y-[2px] cursor-pointer' onClick={() => setSelectContact(false)} />}
+          <h2 className='text-lg font-medium ml-2'>Lorem Ipsum</h2>
+        </div>
+        <p>Message</p>
+      </div>
+      <div className='flex items-center'>
+        <p className='mr-5 md:block hidden'>Lorem Ipsum</p>
+        <div className='relative'>
+          <div className='cursor-pointer' onClick={() => setOpenDropdown(!openDropdown)}>
+            <Avatar size='20px' />
+          </div>
+          <div className={`border-2 transition-transform absolute origin-top translate-y-[10px] top-full right-0 w-[150px] bg-white drop-shadow-xl rounded-md ${openDropdown ? 'scale-y-100' : 'scale-y-0'}`}>
+            <div className='flex items-center p-3 hover:bg-gray-100 cursor-pointer border-b-2 rounded-tl-md rounded-tr-md'>
+              <FaEdit className='mr-2 text-lg' />
+              <p>Edit Profile</p>
+            </div>
+            <div className='flex items-center p-3 hover:bg-gray-100 cursor-pointer rounded-bl-md rounded-br-md'>
+              <MdLogout className='mr-2 translate-y-[2px] text-xl' />
+              <p>Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,40 +1,21 @@
 import { useState } from 'react'
 import { AiOutlineWechat } from 'react-icons/ai'
-import { IoChevronBackOutline } from 'react-icons/io5'
 import ChatInput from './../components/input/ChatInput'
 import SearchForm from './../components/input/SearchForm'
 import MessageContainer from '../components/message/MessageContainer'
 import ContactContainer from '../components/global/ContactContainer'
-import Avatar from './../components/global/Avatar'
+import Header from './../components/global/Header'
 
 const Dashboard = () => {
   const [selectContact, setSelectContact] = useState(false)
 
   return (
     <>
-      <div className='flex items-center justify-between border-b-2 py-1 md:px-12 px-4 sticky top-0'>
-        <div className='flex items-center'>
-          <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt='Inspace' width='35' />
-          <h1 className='md:block hidden font-logo text-lg ml-4'>Inspace</h1>
-        </div>
-        <div className='text-center'>
-          <div className='flex items-center'>
-            {selectContact && <IoChevronBackOutline className='md:hidden block translate-y-[2px] cursor-pointer' onClick={() => setSelectContact(false)} />}
-            <h2 className='text-lg font-medium ml-2'>Lorem Ipsum</h2>
-          </div>
-          <p>Message</p>
-        </div>
-        <div className='flex items-center'>
-          <p className='mr-5 md:block hidden'>Lorem Ipsum</p>
-          <Avatar size='20px' />
-        </div>
-      </div>
+      <Header selectContact={selectContact} setSelectContact={setSelectContact} />
       <div className='md:flex md:static relative overflow-x-hidden'>
         <div className='md:flex-1 md:border-r-2'>
-          <div>
-            <SearchForm placeholder='Search contact ...' />
-            <ContactContainer setSelectContact={setSelectContact} />
-          </div>
+          <SearchForm placeholder='Search contact ...' />
+          <ContactContainer setSelectContact={setSelectContact} />
         </div>
         <div className={`md:flex-[3] md:static transition-all duration-200 absolute top-0 bottom-0 h-[100%] bg-white md:h-[90vh] w-full flex flex-col ${selectContact ? 'right-0' : '-right-[5000px]'}`}>
           {
