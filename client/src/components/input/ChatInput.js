ef378a2b00cbda38b638d4187646d85ff931a7f9
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-02-01 15:22:48 +0700

    Add input type file to photo and attach icon at ChatInput component

--- client/src/components/input/ChatInput.js
+++ client/src/components/input/ChatInput.js
@@ -13,8 +13,14 @@ const ChatInput = () => {
       <form onSubmit={handleSubmit} className='flex items-center justify-between'>
         <input type='text' placeholder='Message here ...' className='outline-0 w-full pr-3' />
         <div className='flex items-center'>
-          <IoMdAttach className='text-xl text-gray-700 mr-3 cursor-pointer' />
-          <HiPhotograph className='text-xl text-gray-700 mr-3 cursor-pointer' />
+          <div className='relative w-[20px] h-[20px] overflow-hidden mr-3'>
+            <input type='file' className='absolute z-10 opacity-0 h=[20px' />
+            <IoMdAttach className='text-xl text-gray-700 absolute top-0 left-0' />
+          </div>
+          <div className='relative w-[20px] h-[20px] overflow-hidden mr-3'>
+            <input type='file' className='absolute z-10 opacity-0 h-[20px]' />
+            <HiPhotograph className='text-xl text-gray-700 absolute top-0 left-0' />
+          </div>
           <FaMicrophone className='text-xl text-gray-700 mr-3 cursor-pointer' />
           <RiSendPlaneFill className='text-xl text-gray-70 cursor-pointer' />
         </div>
