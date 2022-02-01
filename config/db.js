8ee4be38aff95193a664cbf06f7fc26a7302181d
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-02-01 16:01:41 +0700

    Setup server

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectDB = async() => {
+  await mongoose.connect(process.env.MONGO_URL, {
+    autoIndex: false
+  }, err => {
+    if (err) throw err
+    console.log('Successfully connected to MongoDB.')
+  })
+}
+
+module.exports = connectDB
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config({
+  path: './config/.env'
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+connectDB()
+app.listen(process.env.PORT, () => console.log(`Server is running on PORT ${process.env.PORT}.`))
