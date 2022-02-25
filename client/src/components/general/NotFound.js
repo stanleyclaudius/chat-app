8a98a3307b45f677c6a817521d734b19e509bc67
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-02-25 08:56:25 +0700

    Create page not found component

--- client/src/components/general/NotFound.js
+++ client/src/components/general/NotFound.js
@@ -1,7 +1,8 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className='flex items-center justify-center flex-col w-full h-screen'>
+      <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt='Inspace' />
+      <h1 className='font-medium text-3xl mt-5'>Inspace | 404 Not Found</h1>
     </div>
   )
 }
